05c00e7afdd2eb0ef54f90ffb247adfe01ad900c
pthummalapalli <prasadkumar.thummalapalli@va.gov>
2024-01-30 22:30:09 -0700

    added more...

--- src/main/java/practice/ReverseLinkedList.java
+++ src/main/java/practice/ReverseLinkedList.java
@@ -1,4 +1,22 @@
 package practice;
 
+//https://leetcode.com/problems/reverse-linked-list/description/
+
+//TC: O(n)
+//SC: O(1)
 public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+
+        //O(n)
+        while(curr != null) {
+            ListNode nextNode = curr.next; //2; 3
+            curr.next = prev; //1(null); 2(1)
+            prev = curr;//1; 2
+            curr = nextNode; //2; 3
+        }
+
+        return prev;
+    }
 }
